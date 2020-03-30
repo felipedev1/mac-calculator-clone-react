0f57a27ce8c2bb5153dd0724aa3c876d5c35beb1
Felipe <felipe.pereira.dev@gmail.com>
2020-03-30 11:53:44 -0300

    Updates

---
+++ precache-manifest.db40a21eac960d7507abc6f147cc1359.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b7fad3215a8460d87f5582efbe34c3",
+    "url": "/mac-calculator-clone/index.html"
+  },
+  {
+    "revision": "f012f7fbc2f8cc1b071d",
+    "url": "/mac-calculator-clone/static/css/main.39140374.chunk.css"
+  },
+  {
+    "revision": "6c7fc38bcbd5f98c6799",
+    "url": "/mac-calculator-clone/static/js/2.42d7e0b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mac-calculator-clone/static/js/2.42d7e0b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f012f7fbc2f8cc1b071d",
+    "url": "/mac-calculator-clone/static/js/main.345dd834.chunk.js"
+  },
+  {
+    "revision": "a931c42b78d9a6dadac2",
+    "url": "/mac-calculator-clone/static/js/runtime-main.f771bb26.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/mac-calculator-clone/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
